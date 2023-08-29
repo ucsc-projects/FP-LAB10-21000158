f21d458b345b19931c456923504b8020fe1046d9
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2023-08-29 07:13:16 +0530

    Fisnish Q1

--- src/main/scala/Q1.scala
+++ src/main/scala/Q1.scala
@@ -1,3 +1,15 @@
 object Q1 {
 
+  def calculateAverage(temperatures:List[Double]):Double={
+    val convertedTemps=temperatures.map((x)=>(x*9/5)+32);
+    val sum=convertedTemps.reduce((a,b)=>a+b);
+    val count=temperatures.length
+    sum/count
+  }
+  def main(args: Array[String]): Unit = {
+    val temperatures=List(0.0,10.0,20.0,30.0);
+    val averageFahrenheit=calculateAverage(temperatures);
+    println("Average Fahrenheit temperature is "+averageFahrenheit);
+  }
+
 }
